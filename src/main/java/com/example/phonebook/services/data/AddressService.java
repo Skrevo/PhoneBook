8c6673ed6cef21f1bcc17116758606f56be721ae
Дмитрий <Skrevo@outlook.com>
2023-01-02 21:13:52 +0200

    services AddressService and UserService

--- src/main/java/com/example/phonebook/services/data/AddressService.java
+++ src/main/java/com/example/phonebook/services/data/AddressService.java
@@ -1,4 +1,8 @@
 package com.example.phonebook.services.data;
 
+import com.example.phonebook.models.Address;
+
 public interface AddressService {
+
+    Address save(Address address);
 }
--- src/main/java/com/example/phonebook/services/data/UserService.java
+++ src/main/java/com/example/phonebook/services/data/UserService.java
@@ -1,4 +1,17 @@
 package com.example.phonebook.services.data;
 
+import com.example.phonebook.models.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAll();
+
+    User save(User user);
+
+    User findById(Integer id);
+
+    User findByUsername(String username);
+
+    List<User> saveAll(List<User> users);
 }
