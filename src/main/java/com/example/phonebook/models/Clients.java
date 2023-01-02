49724644a87040c07df08c81eb5abd6171a854c4
Дмитрий <Skrevo@outlook.com>
2023-01-02 20:08:10 +0200

    models Clients

---
+++ src/main/java/com/example/phonebook/models/Clients.java
@@ -0,0 +1,28 @@
+package com.example.phonebook.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Clients {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String surname;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 50, nullable = false)
+    private String patronymic;
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+}
