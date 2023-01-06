f27fabe9c966f15e66bd9f28b7f7da86c4e05b4f
Дмитрий <Skrevo@outlook.com>
2023-01-06 10:13:34 +0200

    ClientsController edit

--- src/main/java/com/example/phonebook/ui/controllers/ClientsController.java
+++ src/main/java/com/example/phonebook/ui/controllers/ClientsController.java
@@ -5,7 +5,11 @@ import com.example.phonebook.services.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,5 +24,16 @@ public class ClientsController {
         List<Client> list = clientService.findAll();
         model.addAttribute("clients", list);
         return "clients";
+    }
+
+    @PostMapping("addClientForm")
+    public String addClientForm(@ModelAttribute Client client) {
+        clientService.save(client);
+        return "redirect:clients";
+    }
+
+    @PostMapping("openClientForm")
+    public ModelAndView openClientForm(Integer id){
+        return new ModelAndView("redirect:clientUpdate", new ModelMap("id", id));
     }
 }
